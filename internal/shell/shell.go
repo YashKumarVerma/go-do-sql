8b72e776bc3cbf5b827a85689a8916bae32661e1
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-10-30 03:24:16 +0530

    feat: tableName, add mock data

--- internal/shell/shell.go
+++ internal/shell/shell.go
@@ -1,6 +1,8 @@
 package shell
 
 import (
+	"bufio"
+	"os"
 	"strings"
 
 	ui "github.com/YashKumarVerma/go-lib-ui"
@@ -22,7 +24,7 @@ func autoComplete(document prompt.Document) []prompt.Suggest {
 
 		{Text: "--length:10", Description: "if set, will pass length parameter inside datatype"},
 		{Text: "--unique", Description: "datatype of column as boolean"},
-		{Text: "--foreign:id|users(id)", Description: "id references id of table users"},
+		{Text: "--foreign:users(id)", Description: "id references id of table users"},
 		{Text: "--null", Description: "set to allow null values, else not null by default"},
 		{Text: "--default:val", Description: "default value of field"},
 	}
@@ -31,6 +33,9 @@ func autoComplete(document prompt.Document) []prompt.Suggest {
 
 // ColumnStorage to store entered columns
 var ColumnStorage []string
+
+// TableName to which said columns belong
+var TableName string
 
 // Display shell onto terminal
 func Display() {
@@ -54,10 +59,31 @@ func Display() {
 			}
 		}
 	}
+	// commands to test without entering manually
+	// ColumnStorage = append(ColumnStorage, "--name:id --type:int --auto_increment --primary")
+	// ColumnStorage = append(ColumnStorage, "--name:name --type:string --length:64")
+	// ColumnStorage = append(ColumnStorage, "--name:age --type:int")
+	// ColumnStorage = append(ColumnStorage, "--name:dob --type:datetime")
+	// ColumnStorage = append(ColumnStorage, "--name:mobile --type:string --null")
+	// ColumnStorage = append(ColumnStorage, "--name:country --type:string --length:2 --default:IN")
+	// ColumnStorage = append(ColumnStorage, "--name:aadhar --type:string --length:16 --default:0000000000000000 --unique")
+	// ColumnStorage = append(ColumnStorage, "--name:email --type:string --length:32 --default:unknown@gmail.com --foreign:students(email)")
+
 }
 
 // Initialize command history storage
 func Initialize() {
+	// input relation name from user
+	ui.ContextPrint("package", "Enter table name")
+	ui.ContextPrint("package", "")
+	reader := bufio.NewReader(os.Stdin)
+	relationName, err := reader.ReadString('\n')
+	ui.CheckError(err, "Error reading table/relation name", true)
+	relationName = strings.Trim(relationName, " ")
+	relationName = strings.Trim(relationName, "\n")
+
+	// launch shell for user
 	ui.ContextPrint("spiral_shell", "Launching shell")
 	ColumnStorage = make([]string, 0)
+	TableName = relationName
 }

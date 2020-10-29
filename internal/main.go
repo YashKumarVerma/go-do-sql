1bf8c24c43c6b400f03b0587c7a49d9d90145814
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-10-29 16:56:04 +0530

    chore: fix build script

--- internal/main.go
+++ internal/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/YashKumarVerma/go-do-sql/internal/config"
 	"github.com/YashKumarVerma/go-do-sql/internal/shell"
+	ui "github.com/YashKumarVerma/go-lib-ui"
 )
 
 func main() {
+	ui.ContextPrint("dolphin", "SQL Helper !")
 	config.Initialize()
 	shell.Initialize()
 	shell.Display()
